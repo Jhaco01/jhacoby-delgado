b7ac461161601c3cc1492d1c49de1351d48a9235
Jhacoby K. Delgado <jkeithd28@gmail.com>
2022-09-19 17:09:40 -0300

    style added to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -24,6 +24,33 @@ export const Contact = () => {
                             idx={15}
                         />
                     </h1>
+                    <p>
+                        I'm interested in jobs oportunities for juniors devs or trainee devs,
+                        mainly to start earning some experience in the area of JavaScript, HTML, CSS 
+                        and React; however, if is there any oportunity related or not with those, you 
+                        can contact me with the form below.  
+                    </p>
+                    <div className="contact-form">
+                        <form>
+                            <ul>
+                                <li className="half"> 
+                                    <input type="text" name="name" placeholder="Name" required /> 
+                                </li>
+                                <li className="half">
+                                    <input type="email" name="email" placeholder="Email" required />
+                                </li>
+                                <li>
+                                    <input placeholder="Subject" type="text" name="subject" required />
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message" name="message" required></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="Send" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
             < Loader type="pacman" />
